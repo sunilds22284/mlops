c1e39645c0138d4f3f81eab39629f7c945121256
sunilcristo <92639793+sunilcristo@users.noreply.github.com>
2023-10-14 18:47:08 +0530

    Add files via upload

--- deploy.py
+++ deploy.py
@@ -16,12 +16,12 @@ ms = project.get_model_serving()
 
 dataset_api = project.get_dataset_api()
 
-uploaded_file_path = dataset_api.upload("/content/predictor.py", "Models", overwrite=True)
+uploaded_file_path = dataset_api.upload("./predictor.py", "Models", overwrite=True)
 predictor_script_path = os.path.join("/Projects", project.name, uploaded_file_path)
 
 deployment_name = "ladeployment"
 my_deployment = my_model.deploy(name=deployment_name,
-                                          description="Deployment that search for item candidates and scores them based on customer metadata",
+                                          description="Loan Approval",
                                           script_file=predictor_script_path,
                                           resources={"num_instances": 0})
 my_deployment.start()
