7e443beb31807ea0ffd93ef886a6994e44283da1
sunilcristo <92639793+sunilcristo@users.noreply.github.com>
2023-10-14 18:42:46 +0530

    Add files via upload

--- deploy.py
+++ deploy.py
@@ -9,21 +9,21 @@ project = hopsworks.login()
 # get Hopsworks Model Registry handle
 mr = project.get_model_registry()
 
-la_model = mr.get_model("loan_approval_v3", version=1)
+my_model = mr.get_model("loan_approval_v3", version=1)
 
 # get Hopsworks Model Serving handle
 ms = project.get_model_serving()
 
 dataset_api = project.get_dataset_api()
 
-uploaded_file_path = dataset_api.upload("predictor.py", "Models", overwrite=True)
+uploaded_file_path = dataset_api.upload("/content/predictor.py", "Models", overwrite=True)
 predictor_script_path = os.path.join("/Projects", project.name, uploaded_file_path)
 
-deployment_name = "loan_approval_v3"
-my_deployment = la_model.deploy(name=deployment_name,
-                                description="Deployment which can do loan approval prediction",
-                                script_file=predictor_script_path,
-                                resources={"num_instances": 0})
+deployment_name = "ladeployment"
+my_deployment = my_model.deploy(name=deployment_name,
+                                          description="Deployment that search for item candidates and scores them based on customer metadata",
+                                          script_file=predictor_script_path,
+                                          resources={"num_instances": 0})
 my_deployment.start()
 
 
